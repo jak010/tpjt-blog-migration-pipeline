4cf5acb106cca6f010dda8d48ead753513075e84
jak010 <bluetoon@naver.com>
2026-01-04 16:40:11 +0900

    refactor(blog-migration): 티스토리 파싱 로직 및 메인 흐름 리팩토링
    `main.py`에서 티스토리 블로그 게시글의 내용, 생성일, 제목 등을 파싱하고 LLM을 통해 변환하는 로직을 `libs/tistory_parser.py`로 분리했습니다. 또한, `main.py`의 흐름을 간결하게 정리했습니다. `db.json` 파일의 `is_saved` 값을 `null`에서 `true`로 변경했습니다.

--- libs/tistory_parser.py
+++ libs/tistory_parser.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
-from typing import List, Union
-from typing import TYPE_CHECKING
+from typing import List, Union, TYPE_CHECKING
 
 from bs4 import Tag, NavigableString
 
--- main.py
+++ main.py
@@ -1,39 +1,39 @@
-# from libs.engine import Engine
-# from libs.tistory_checker import TistoryRSSChecker
-# from libs.tistory_parser import TistoryContentParser
-# from storage.dao import FileDao
+from libs.engine import Engine
+from libs.llm.gemini import GeminiLLM
+from libs.tistory_checker import TistoryRSSChecker
+from libs.tistory_parser import TistoryContentParser
+from storage.dao import FileDao
 
-# from libs.export.template import MarkDownExportTemplate
+from libs.export.template import MarkDownExportTemplate
 
 # Tistory Parser
-# c = TistoryRSSChecker()
-#
-# file_dao = FileDao()
-# r = file_dao.get_accessable_post(depth=3)
+c = TistoryRSSChecker()
 
-# contents = [
-#     TistoryContentParser(
-#         Engine.initialize(
-#             f"https://jakpentest.tistory.com/{post.get('id')}"
-#         )
-#     )
-#     for post in r
-# ]
+file_dao = FileDao()
+r = file_dao.get_accessable_post(depth=3)
+
+contents = [
+    TistoryContentParser(
+        Engine.initialize(
+            f"https://jakpentest.tistory.com/{post.get('id')}"
+        )
+    )
+    for post in r
+]
+
+content = contents[0]
+
+file_created_at = content.get_created_at()
+file_content = content.get_content_source()
 
 
-# file_created_at = content.get_created_at()
-# file_content = content.get_content_source()
+# content make
+llm = GeminiLLM()
+result = llm.execute('\n\n'.join(file_content))
 
-# file_created_at = content.get_created_at()
-# file_content = content.get_content_source()
-
-# # content make
-# llm = GeminiLLM()
-# result = llm.execute('\n\n'.join(file_content))
-#
-# # Markdown Export
-# exporter = MarkDownExportTemplate(
-#     file_name=content.get_title(),
-#     file_content=result.text
-# )
-# exporter.execute()
+# Markdown Export
+exporter = MarkDownExportTemplate(
+    file_name=content.get_title(),
+    file_content=result.text
+)
+exporter.execute()
